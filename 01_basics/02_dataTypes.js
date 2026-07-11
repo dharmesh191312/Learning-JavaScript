55b4c5398f9163c50e6553bb196f3f9699431ed6
Dharmesh Singhal <54994055+dharmesh191312@users.noreply.github.com>
2026-07-11 09:17:39 +0000

    02_dataTypes file done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3); //we are using node.js, not browser, that's why it won't run here.
+
+console.log(3 
+    +
+      3) //code readability should be high
+
+console.log("Dharmesh");
+
+let name = "dharmesh";
+let age = 25;
+let isLoggedIn = true;
+let state;
+
+// number => 2
+// bigInt => 9007199254740995n
+// string => "Dharmesh"
+// boolean => true/false
+// undefined => let state;
+// null 
+// symbol => unique
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
