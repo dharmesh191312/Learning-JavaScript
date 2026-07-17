1c12f78eb8a6d7e85b5e49fac894b865bc751eef
Dharmesh Singhal <54994055+dharmesh191312@users.noreply.github.com>
2026-07-17 06:30:07 +0000

    Add string manipulation examples in 05_strings.js

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,13 @@
+const name = "Dharmesh";
+const repoCount = 50;
+
+// console.log(name + repoCount + " value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}.`);
+
+const gameName = new String('dharmesh-ds-com');
+console.log(gameName);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
